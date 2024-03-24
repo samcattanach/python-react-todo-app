551d8bd64efee3642e9b582ed9cd19de2d719189
samcattanach <s-cattanach@live.co.uk>
2024-03-24 20:10:34 +0100

    test run.py

--- api/run.py
+++ api/run.py
@@ -1,7 +1,15 @@
 # app entry point
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
+
+if __name__ == "__main__":
     app.run(debug=True)
