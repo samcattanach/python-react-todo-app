b9c7853bb3a1e3a46e0139e444cc8de250b4d449
samcattanach <s-cattanach@live.co.uk>
2024-03-25 20:11:30 +0100

    deployment changes

--- api/start.sh
+++ api/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start Cloud SQL Proxy
-./cloud-sql-proxy flask-react-todo-app:europe-west2:todo-app &
+./cloud_sql_proxy -instances=flask-react-todo-app:europe-west2:todo-app=tcp:5432 &
 
 # Start Flask app
 exec python3 -m flask run --host=0.0.0.0
